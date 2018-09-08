14ff83f118842a2428aebdfa01ab679b007408bd
GuavaLand <yuan_yin@outlook.com>
2018-09-08 17:02:58 -0400

    Generate community boolean mask and steady state community composition SSMatrix

--- varyingCoefficientLinear.R
+++ varyingCoefficientLinear.R
@@ -10,7 +10,7 @@ glv <- function(t, x, params){
 
 #Define integration method
 n.integrate <- function(time, init.x, model, params){
-  as.data.frame(ode(init.x, time, model, params))
+  as.data.frame(lsoda(init.x, time, model, params))
 }
 
 
@@ -43,3 +43,34 @@ dat <- n.integrate(0:10, init.x, glv, list(alpha=alpha, c0=c0, ck1 = ck1, ck2 = 
 
 #Plot
 matplot(x=dat$time, y=dat[,-1], typ='b', xlab='time', ylab='Absolute abundance')
+
+
+
+###############################################################################
+#Uncomment to execute the following section
+
+#Generate 2^N communities where species in each community (max possible N) are present or absent
+###############################################################################
+
+#Generate boolean mask of 2^N x N matrix
+repeatBinaryNTimes <- rep(list(c(0,1)),N)
+mask <- expand.grid(repeatBinaryNTimes)
+
+#Loop through 2^N to apply each row in mask to initial abundance, solve ode, and retrieve final abundance
+SSMatrix <- as.data.frame(matrix(nrow = 2^N, ncol = N))
+colnm <- c(1:10)
+colnames(SSMatrix) <- colnm
+for (i in 1:2^N) {
+  init <- init.x*mask[i,]
+  init <- as.numeric(init)
+  dat <- n.integrate(0:10, init, glv, list(alpha=alpha, c0=c0, ck1 = ck1, ck2 = ck2))
+  SSMatrix[i,] <- dat[nrow(dat),2:(N+1)]
+}
+
+
+
+
+
+
+
+
