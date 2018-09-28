e81595edb6fc99af0d081187fc69d2a17e5c254c
GuavaLand <yuan_yin@outlook.com>
2018-09-28 02:06:21 -0400

    Working on main funtion

--- DensityMediated_varyingCoefficientLinear.R
+++ DensityMediated_varyingCoefficientLinear.R
@@ -73,8 +73,8 @@ for (i in 1:N) {
 init.x <- floor(runif(N, min = 1, max = 10))/10
 
 #Solve the ode
-dat <- n.integrate(0:400, init.x, glv, list(alpha=alpha, c0=c0, l=l))
-dat1 <- n.integrate(0:400, init.x, glv1, list(alpha=alpha, c0=c0))
+dat <- n.integrate(0:500, init.x, glv, list(alpha=alpha, c0=c0, l=l))
+dat1 <- n.integrate(0:500, init.x, glv1, list(alpha=alpha, c0=c0))
 #Plot
 
 matplot(x=dat$time, y=dat[,-1], typ='b', xlab='time', ylab='Absolute abundance', main=paste('Modified GLV-density', N,'species'))
--- main.R
+++ main.R
@@ -7,33 +7,52 @@ numTrial = 1000
 HOI_SS10 <-  matrix(nrow = numTrial, ncol = N)
 GLV_SS10 <-  matrix(nrow = numTrial, ncol = N)
 
-for (i in 1:numTrial) {
+for (trial in 1:numTrial) {
   source('DensityMediated_varyingCoefficientLinear.R')
   #Modified GLV
-  for (j in 2:ncol(dat)) {
-    HOI_SS10[1,j-1] <-  FindSS(dat[,j])
+  for (species in 2:ncol(dat)) {
+    HOI_SS10[trial,species-1] <-  FindSS(dat[,species])
   }
   
   #Original GLV
-  for (j in 2:ncol(dat1)) {
-    GLV_SS10[1,j-1] <-  FindSS(dat[,j])
+  for (oSpecies in 2:ncol(dat1)) {
+    GLV_SS10[trial,oSpecies-1] <-  FindSS(dat1[,oSpecies])
   }
 }
+
 
 
 N <-  20
 HOI_SS20 <-  matrix(nrow = numTrial, ncol = N)
 GLV_SS20 <-  matrix(nrow = numTrial, ncol = N)
 
-for (i in 1:numTrial) {
+for (trial in 1:numTrial) {
   source('DensityMediated_varyingCoefficientLinear.R')
   #Modified GLV
-  for (j in 2:ncol(dat)) {
-    HOI_SS20[1,j-1] <-  FindSS(dat[,j])
+  for (species in 2:ncol(dat)) {
+    HOI_SS20[trial,species-1] <-  FindSS(dat[,species])
   }
   
   #Original GLV
-  for (j in 2:ncol(dat1)) {
-    GLV_SS20[1,j-1] <-  FindSS(dat[,j])
+  for (oSpecies in 2:ncol(dat1)) {
+    GLV_SS20[trial,oSpecies-1] <-  FindSS(dat1[,oSpecies])
+  }
+}
+
+
+N <-  30
+HOI_SS30 <-  matrix(nrow = numTrial, ncol = N)
+GLV_SS30 <-  matrix(nrow = numTrial, ncol = N)
+
+for (trial in 1:numTrial) {
+  source('DensityMediated_varyingCoefficientLinear.R')
+  #Modified GLV
+  for (species in 2:ncol(dat)) {
+    HOI_SS30[trial,species-1] <-  FindSS(dat[,species])
+  }
+  
+  #Original GLV
+  for (oSpecies in 2:ncol(dat1)) {
+    GLV_SS30[trial,oSpecies-1] <-  FindSS(dat1[,oSpecies])
   }
 }
