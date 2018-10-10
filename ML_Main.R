312a2375fb5b561dd7b4e1190c0f29e11f58b98c
GuavaLand <yuan_yin@outlook.com>
2018-10-10 14:45:58 -0400

    Train random forest with init_mask matrix and each species column (N models trained)

--- ML_Main.R
+++ ML_Main.R
@@ -22,7 +22,7 @@ l <- simulationParam$l
 init <- simulationParam$init
 
 #rows of mask: # of data points
-M = 100
+M = 1000
 mask <- matrix(sample(c(0,1),M*N,replace=TRUE), nrow = M, ncol = N)
 
 #count unique number of rows
@@ -50,6 +50,7 @@ SS[which(SS < 0.001)] = 0
 
 ############################################
 # input: init_mask
+# ML
 # output: SS
 #
 #each row is an entry of data (determined by M)
@@ -60,6 +61,25 @@ SS[which(SS < 0.001)] = 0
 train_x = as.data.frame(init_mask)
 train_y = as.data.frame(SS)
 
-mod <- apply(train_y, MARGIN = 2, function(x){randomForest(x ~., train_x)}) 
+if (any(is.na(train_y))) {
+  #Row index where the row contains NA
+  NA_containing_rows = unique(which(is.na(train_y),arr.ind = TRUE)[,1])
+  #Remove NA containing rows
+  train_x = train_x[-NA_containing_rows,]
+  train_y = train_y[-NA_containing_rows,]
+}
+
+#Train ONE rf train_x to each column in train_y
+rf_species = list()
+for (i in 1:N) {
+  #for species i, if train_x starts with 0, train_y will be 0 
+  #exclude 0
+  null_begining = which(train_x[,i] == 0)
+  train_x_filtered = train_x[-null_begining,]
+  train_y_filtered = train_y[-null_begining,]
+  
+  data = cbind(train_x_filtered,y = train_y_filtered[,i])
+  rf_species[[i]] = randomForest(y ~ ., data = data)
+}
 
 
